84d5917361d721f8ab6f134c1419a270a9204aeb
Daniel Araya <39106936+danielaraya71@users.noreply.github.com>
2024-10-12 15:51:02 -0600

    First tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,5 +1,36 @@
 const { Web3 } = require('web3');
 const ganache = require('ganache');
 const assert = require('assert');
+const web3 = new Web3(ganache.provider()); 
 
-//
+// Testing with Mocha
+
+class Car {
+    park(){
+        return 'stopped'
+    }
+
+    drive(){
+        return 'vroom'
+    }
+}
+
+//Tests the Car functions
+//describes lets you group several it functions
+
+let car;
+
+beforeEach (()=>{
+    car = new Car();
+});
+
+describe('Car', () => {
+    
+    it('can park', () => {
+        assert.equal(car.park(), 'bah'); //This one will fail
+    });
+
+    it('can drive', () => {
+        assert.equal(car.drive(), 'vroom'); //This one will pass
+    });
+});
